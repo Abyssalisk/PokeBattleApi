7c505180b6ef1365e95197706a840588562b7ba9
Spencer Rosenvall <srosenvall@itnint.com>
2019-04-14 14:04:13 -0600

    set up done.

--- PokeApiBattleSimulator/PokeApiBattleSimulator/Program.cs
+++ PokeApiBattleSimulator/PokeApiBattleSimulator/Program.cs
@@ -8,8 +8,57 @@ namespace PokeApiBattleSimulator
 {
     class Program
     {
+        Random r;
+
+        // method to get a pokemon
+
+
+        // sub method to get moves of a pokemon
+
+
+        // createPokemon (populates the values for the pokemon from the response
+
+
+        // doDamage(pokemon1, pokemon2)
+
+
+        public Program()
+        {
+            r = new Random(400);
+        }
+
+
+
+        class Pokemon
+        {
+
+            class Move
+            {
+                string name { get; set; }
+                int damage { get; set; }
+            }
+
+            string name { get; set; }
+            int health { get; set; }
+            List<Move> moves { get; set; }
+
+
+
+        }
+
+
+
+
+
         static void Main(string[] args)
         {
+
+            // create pokemon 1, 2
+            // set values
+
+
+
+
         }
     }
 }
